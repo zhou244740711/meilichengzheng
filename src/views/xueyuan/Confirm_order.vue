127f5bde7730a191aa6490b906f3d707c813bbec
kimi <244740711@qq.com>
2021-03-30 22:52:01 +0800

    #购买课程静态页面完成

---
+++ src/views/xueyuan/Confirm_order.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="xueyuanindex clearfix">
+
+    <div class="confirm_order_card_list">
+      <div class="stydy_card confirm_order_card">
+        <div class="card_header">
+          <span class="number">编号：202120200214</span>
+          <span class="tip">课程包</span>
+        </div>
+        <div class="card_main">
+          <p class="title">名称课程包名称…</p>
+          <div class="row">
+          <span class="col t1">
+            共 <b>4</b> 课时
+            <span class="tag">驻镇规划师</span>
+          </span>
+            <span class="t2">¥10</span>
+          </div>
+        </div>
+      </div>
+      <div class="stydy_card confirm_order_card">
+        <div class="card_header">
+          <span class="number">编号：202120200214</span>
+        </div>
+        <div class="card_main">
+          <p class="title">名称课程包名称…</p>
+          <div class="row">
+          <span class="col t1">
+            共 <b>4</b> 课时
+            <span class="tag">驻镇规划师</span>
+          </span>
+            <span class="t2">¥10</span>
+          </div>
+        </div>
+      </div>
+      <div class="stydy_card confirm_order_card">
+        <div class="card_header">
+          <span class="number">编号：202120200214</span>
+        </div>
+        <div class="card_main">
+          <p class="title">名称课程包名称…</p>
+          <div class="row">
+          <span class="col t1">
+            共 <b>4</b> 课时
+            <span class="tag">驻镇规划师</span>
+          </span>
+            <span class="t2">¥10</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="confirm_list">
+      <div class="item row row-center">
+        <span class="title">优惠劵</span>
+        <div class="col text" @click="choseCoupon()">9折</div>
+        <i class="iconfont icon-jiantou-you"></i>
+      </div>
+      <div class="item row row-center">
+        <span class="title">发票</span>
+        <div class="col text greg9" @click="showinvoice()">填写发票</div>
+        <i class="iconfont icon-jiantou-you"></i>
+      </div>
+    </div>
+    <div class="confirm_tip">提示：请谨慎购买，购买课程进行了课程学习，将不予退费。</div>
+
+    <div style="height: 46px;"></div>
+    <div class="confirm_footer row row-center">
+      <div class="col text">
+        <p class="t1">实付金额：<span class="t3">¥10</span></p>
+        <p class="t2">优惠金额：¥3</p>
+      </div>
+      <div class="button">确认支付</div>
+    </div>
+
+    <invoice ref="invoice"></invoice>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Invoice from "./invoice";
+export default {
+  name: 'ConfirmOrder',
+  components: {Invoice},
+  data() {
+    return {
+    }
+  },
+  created: function () {
+  },
+  methods: {
+    choseCoupon () {
+      this.$router.push({name: 'Coupon'})
+    },
+    showinvoice () {
+      this.$refs.invoice.showModal()
+    }
+  }
+}
+</script>
---
+++ src/views/xueyuan/invoice.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="invoice" v-show="show">
+    <div class="invoice_modal">
+      <div class="invoice_header">填写发票</div>
+      <div class="invoice_main">
+        <div class="invoice_list">
+          <div class="item row row-center">
+            <span class="title">发票类型：</span>
+            <z-select class="col noborder" v-model="invoicedata.type" :options="typelist"></z-select>
+          </div>
+          <div class="item">
+            <mt-radio
+                class="list_radio"
+                v-model="invoicedata.stype"
+                :options="slist">
+            </mt-radio>
+          </div>
+          <div class="item row row-center">
+            <span class="title">发票抬头*：</span>
+            <input type="text" class="col input">
+          </div>
+          <div class="item row row-center">
+            <span class="title">纳税人识别号：</span>
+            <input type="text" class="col input">
+          </div>
+          <div class="item row row-center">
+            <span class="title">发票内容：</span>
+            <input type="text" class="col input">
+          </div>
+        </div>
+
+        <div class="z-button">保存</div>
+
+      </div>
+    </div>
+    <div class="invoice_bg" @click="show = false"></div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import ZSelect from "../../components/ZSelect";
+export default {
+  name: 'invoice',
+  components: {ZSelect},
+  data() {
+    return {
+      show: false,
+      invoicedata: {
+        type: '电子'
+      },
+      slist: ['个人', '企业'],
+      typelist: [
+        {
+          label: '电子',
+          value: '电子'
+        }
+      ]
+    }
+  },
+  created: function () {
+  },
+  methods: {
+    showModal () {
+      this.show = true
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.invoice{
+  .invoice_modal{
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    z-index: 20;
+    background: #FFFFFF;
+    .invoice_header{
+      font-weight: 500;
+      font-size: 16px;
+      color: #333333;
+      height: 45px;
+      line-height: 45px;
+      text-align: center;
+    }
+    .invoice_main{
+      padding: 15px;
+
+      .invoice_list{
+        .item{
+          border-bottom: solid 1px #EEEEEE;
+
+          .title{
+            white-space: nowrap;
+            margin-right: 15px;
+            width: 100px;
+          }
+          .z-select{
+            width: auto;
+          }
+          .list_radio{
+            padding: 10px 0;
+          }
+        }
+      }
+      .z-button{
+        background: linear-gradient(146deg, #10A14C 0%, #50D08F 100%);
+        border-radius: 22px;
+        height: 44px;
+        line-height: 44px;
+        font-size: 20px;
+        text-align: center;
+        margin: 15px 20px;
+      }
+    }
+  }
+  .invoice_bg{
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 10;
+    background: rgba(0, 0, 0, 0.2);
+  }
+}
+</style>
